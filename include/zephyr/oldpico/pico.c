655f79c781a3274c3528709c83b1cc5c3b8126af
ihux <hugo.pristauz@gmail.com>
2024-03-02 19:58:15 +0100

    cleanup

--- include/zephyr/oldpico/pico.c
+++
@@ -1,5 +0,0 @@
-// pico/pico.c - C definition of pico API
-
-#include "pico/pico.h"
-
-pico_api pico = PICO_API();  // declaration of pico API
--- include/zephyr/oldpico/version.h
+++
@@ -1,7 +0,0 @@
-// pico/version.h - pico version
-#ifndef __PICO_VERSION__
-#define __PICO_VERSION__
-
-  #define PI_VERSION "v1.0.0"
-
-#endif // __PICO_VERSION__
